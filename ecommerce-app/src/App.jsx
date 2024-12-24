43b3511d68f99000f0d7412b169a844340dc9b51
Shubhjot Singh <shubhjot@browserstack.com>
2024-12-24 15:10:38 +0530

    Fifth commit added feedback changes

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductsPage from './EcommerceComponent/index';
-import MyCart from './EcommerceComponent/MyCart';
-import ProductDetailsPage from './EcommerceComponent/ProductDetailsPage';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import Checkout from './EcommerceComponent/CheckoutComponent';
-import Orders from './EcommerceComponent/orders';
+
+const ProductsPage = lazy(() => import('./EcommerceComponent/index'));
+const MyCart = lazy(() => import('./EcommerceComponent/MyCart'));
+const ProductDetailsPage = lazy(() => import('./EcommerceComponent/ProductDetailsPage'));
+const Checkout = lazy(() => import('./EcommerceComponent/CheckoutComponent'));
+const Orders = lazy(() => import('./EcommerceComponent/orders'));
 
 function App() {
   return (
     <Provider store={store}>
-    <Router>
-      <div className="container mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<ProductsPage />} />
-          <Route path="/productDetails" element={<ProductDetailsPage />} />
-          <Route path="/cart" element={<MyCart />} />
-          <Route path="/checkout" element={<Checkout/>} />
-          <Route path="/orders" element={<Orders/>} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div className="container mx-auto p-4">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<ProductsPage />} />
+              <Route path="/productDetails" element={<ProductDetailsPage />} />
+              <Route path="/cart" element={<MyCart />} />
+              <Route path="/checkout" element={<Checkout />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </Suspense>
+        </div>
+      </Router>
     </Provider>
   );
 }
